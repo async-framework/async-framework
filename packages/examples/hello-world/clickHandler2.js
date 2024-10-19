a0c47caed1a313a30ab238ebe7df846f73aa0bb9
PatrickJS <github@patrickjs.com>
2024-10-19 13:01:47 -0700

    style: lint

--- packages/examples/hello-world/clickHandler2.js
+++ packages/examples/hello-world/clickHandler2.js
@@ -1,7 +1,7 @@
 // clickHandler2.js
 let count = 0;
 
-export default function clickHandler2({ element, event }) {
+export default function clickHandler2({ element }) {
   count++
   element.textContent = `Clicked Me! ${count}`;
   return count
