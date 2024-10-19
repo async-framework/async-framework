5fd5e7b5cb7535f73d9defd8a8c7b3a7e280730a
PatrickJS <github@patrickjs.com>
2024-10-19 15:33:29 -0700

    feat: custom origin and base path

--- packages/examples/hello-world/handlerRegistry.js
+++ packages/examples/hello-world/handlerRegistry.js
@@ -2,7 +2,9 @@
 export class HandlerRegistry {
   constructor() {
     this.registry = new Map();
-    this.splitIndex = ',';
+    this.splitIndex = ",";
+    this.basePath = "./";
+    this.origin = "";
   }
 
   /**
@@ -14,22 +16,31 @@ export class HandlerRegistry {
    */
   async getHandler(scriptPath) {
     if (this.registry.has(scriptPath)) {
+      // console.log('HandlerRegistry.getHandler: returning cached handler for', scriptPath);
       return this.registry.get(scriptPath);
     }
 
     try {
-      const module = await import(`./${scriptPath}`);
+      // console.log('HandlerRegistry.getHandler: loading async handler at', scriptPath);
+      const module = await import(
+        `${this.origin}${this.basePath}${scriptPath}`
+      );
       const handler = module.default;
-      if (typeof handler === 'function') {
+      if (typeof handler === "function") {
         this.registry.set(scriptPath, handler);
         return handler;
       } else {
+        console.error(
+          `HandlerRegistry.getHandler: Handler at ${scriptPath} is not a function.`,
+        );
         throw new Error(`Handler at ${scriptPath} is not a function.`);
       }
     } catch (error) {
-      console.error(`Failed to load handler at ${scriptPath}:`, error);
+      console.error(
+        `HandlerRegistry.getHandler: Failed to load handler at ${scriptPath}:`,
+        error,
+      );
       throw error;
     }
   }
 }
-
