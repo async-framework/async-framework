953971fdace4984ee98338e64c3f7d6f9c1d8480
PatrickJS <github@patrickjs.com>
2024-10-22 07:06:47 -0700

    refactor: todo

--- packages/examples/2-todo-app/updateTodoList.js
+++ packages/examples/2-todo-app/updateTodoList.js
@@ -13,17 +13,21 @@ export default function updateTodoList({ event, element }) {
           <li class="py-4 flex items-center justify-between ${
         todo.completed ? "bg-gray-50" : ""
       }">
-            <div class="flex items-center flex-1">
+            <div
+              class="flex items-center flex-1"
+              data-id="${todo.id}"
+              on:click="prevent-default.js, toggleTodo.js"
+            >
               <input
                 class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
                 type="checkbox"
                 ${todo.completed ? "checked" : ""}
-                on:change="toggleTodo.js"
+                on:change="prevent-default.js, toggleTodo.js"
                 data-id="${todo.id}"
               >
-              <label class="ml-3 block text-gray-900 flex-grow ${
-        todo.completed ? "line-through text-gray-500" : ""
-      }">${todo.text}</label>
+              <label class="ml-3 block text-gray-900 flex-grow ${todo.completed ? "line-through text-gray-500" : ""}">
+                ${todo.text}
+              </label>
             </div>
             <button
               class="ml-2 text-red-600 hover:text-red-800 focus:outline-none"
