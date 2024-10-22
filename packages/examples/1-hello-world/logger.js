73623f0eb21e4e7fa2910b3101a7a3fa6dbe3a3e
PatrickJS <github@patrickjs.com>
2024-10-22 07:00:26 -0700

    style: logger

--- packages/examples/1-hello-world/logger.js
+++ packages/examples/1-hello-world/logger.js
@@ -1,5 +1,5 @@
 // logger.js
 
 export default function logger({ element, event, value }) {
-  console.log("logger:", { element, event, value });
+  console.log("logger:", { element: element.tagName, event: event.type, value });
 }
